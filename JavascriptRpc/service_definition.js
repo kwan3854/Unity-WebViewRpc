d0f0d02aef13d22d313082d129aa433c4479c86e
KwanJoong Lee <leekwanjung@gmail.com>
2025-02-13 16:30:49 +0900

    Remove JavaScript RPC implementation
    Deleted the entire JavaScript RPC framework, including:
    - Package configuration
    - Encoding/decoding logic for RPC envelopes
    - Client and server classes for handling requests and responses
    - Utility functions for base64 encoding/decoding

--- JavascriptRpc/service_definition.js
+++
@@ -1,8 +0,0 @@
-export class ServiceDefinition {
-    constructor() {
-      // methodName -> handlerFn
-      // handlerFn: (requestBytes: Uint8Array) => Uint8Array
-      this.methodHandlers = {};
-    }
-  }
-  
